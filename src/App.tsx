0c0d283d56103a8be58cd2a5e38e8e797864e967
Tandurin <dennisvlaanderen@gmail.com>
2021-09-12 00:32:25 +0200

    React + Tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const user = {
+  firstName: 'Dennis',
+  lastName: 'Vlaanderen'
+};
+
+function formatName(user: any) {
+  return user.firstName + " " + user.lastName;
+}
+
+const name = 'Dennis Vlaanderen';
+const element = <h1>Hello, {formatName(user)}</h1>;
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    element
   );
 }
 
